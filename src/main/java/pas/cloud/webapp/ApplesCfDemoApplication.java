ede040e2640fc632b2ef0f0ddb92ae7b1b05fb18
Iwan Winoto <iwinoto@gmail.com>
2016-11-09 03:19:40 +1100

    Eclipse project config Deployable to Liberty

--- src/main/java/pas/cloud/webapp/ApplesCfDemoApplication.java
+++ src/main/java/pas/cloud/webapp/ApplesCfDemoApplication.java
@@ -3,6 +3,7 @@ package pas.cloud.webapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.MessageSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
@@ -11,21 +12,21 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ComponentScan
 @EnableAutoConfiguration
-@SpringBootApplication(exclude = MessageSourceAutoConfiguration.class)
-public class ServletInitializer extends SpringBootServletInitializer {
-
-	/*
+// Code for embedded Tomcat deployment
+/* 
 public class ApplesCfDemoApplication {
 
-    public static void main(String[] args)
-    {
-        SpringApplication.run(ApplesCfDemoApplication.class, args);
-    }
+	public static void main(String[] args)
+	{
+    	SpringApplication.run(ApplesCfDemoApplication.class, args);
+	}
 }
 */
-	
-	   @Override
-	   protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-	      return application.sources(ApplesCfDemoApplication.class);
-	   }
+// Code for deployment or WAR to WLP
+@SpringBootApplication(exclude = MessageSourceAutoConfiguration.class)
+public class ApplesCfDemoApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ApplesCfDemoApplication.class);
 	}
+}
