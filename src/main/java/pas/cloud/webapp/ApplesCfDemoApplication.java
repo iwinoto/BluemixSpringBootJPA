6b7c8227c806c9c8c1f7594d0028c642422a7d83
Iwan Winoto <iwinoto@gmail.com>
2016-11-08 17:39:31 +1100

    Deploying to Liberty, cut 1

--- src/main/java/pas/cloud/webapp/ApplesCfDemoApplication.java
+++ src/main/java/pas/cloud/webapp/ApplesCfDemoApplication.java
@@ -2,12 +2,19 @@ package pas.cloud.webapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.MessageSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan
 @EnableAutoConfiguration
+@SpringBootApplication(exclude = MessageSourceAutoConfiguration.class)
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	/*
 public class ApplesCfDemoApplication {
 
     public static void main(String[] args)
@@ -15,3 +22,10 @@ public class ApplesCfDemoApplication {
         SpringApplication.run(ApplesCfDemoApplication.class, args);
     }
 }
+*/
+	
+	   @Override
+	   protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	      return application.sources(ApplesCfDemoApplication.class);
+	   }
+	}
